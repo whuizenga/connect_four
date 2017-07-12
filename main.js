ef6dd38b9cf60bd5d10620c24e34bc250ca44b91
whuizenga <will.huizenga@gmail.com>
2017-07-12 12:32:21 -0400

    clicking on a column returns that column

--- main.js
+++ main.js
@@ -1,39 +1,45 @@
-$(function() {
+$(function () {
     var GameBoard = {
-        // board = [
-        //     [0,0,0,0,0,0,0],
-        //     [0,0,0,0,0,0,0],
-        //     [0,0,0,0,0,0,0],
-        //     [0,0,0,0,0,0,0],
-        //     [0,0,0,0,0,0,0],
-        //     [0,0,0,0,0,0,0],
-        // ]
+        board: [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ],
+
+        //Designates which player is taking a turn. 1 if player 1 turn, 2 if player 2 turn.
+        turn: 1,
+        
+        dropChip: function(column){
+            var columnToDrop = column.data("column");
+            console.log(columnToDrop);
+        }
     };
 
     var ClickHander = {
-    //     click = function(){
-    //         $(".game-square").on("click", function(event){
-    //     $(this).css("background-color", "black");
-    // });
+        click: function (column) {
+            var getTarget = column;
+            GameBoard.dropChip(getTarget);
         }
-    
+    };
 
-    $(".game-square").on("click", function(event){
-        $(this).css("background-color", "black");
-    });
-
-    var createBoard = function(){
-        console.log("running function createBoard()");
-        for(var i = 0; i < 7; i ++){
-            console.log("creating new column");
+    var createBoard = function () {
+        for (var i = 0; i < 7; i++) {
             var $newColumn = $("<div>");
-            for(var n = 0; n < 6; n++){
-                console.log("creating new square");
+            for (var n = 0; n < 6; n++) {
                 var $newSquare = $("<div>");
                 $newSquare.addClass("game-square");
+                $newSquare.data("column",i);
+                $newSquare.data("row", n);
                 $newColumn.append($newSquare);
             };
+            $newColumn.data("column",i);
             $newColumn.addClass("game-column");
+            $newColumn.on("click", function (event) {
+                ClickHander.click(this);
+            });
             $("#game-board").append($newColumn);
         };
     };
