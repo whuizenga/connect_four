afb82f9d1f30ecb427df886b279623180a61071f
whuizenga <will.huizenga@gmail.com>
2017-07-12 11:53:51 -0400

    created the game board using javascript

--- main.js
+++ main.js
@@ -11,10 +11,32 @@ $(function() {
     };
 
     var ClickHander = {
-
-    }
+    //     click = function(){
+    //         $(".game-square").on("click", function(event){
+    //     $(this).css("background-color", "black");
+    // });
+        }
+    
 
     $(".game-square").on("click", function(event){
         $(this).css("background-color", "black");
     });
+
+    var createBoard = function(){
+        console.log("running function createBoard()");
+        for(var i = 0; i < 7; i ++){
+            console.log("creating new column");
+            var $newColumn = $("<div>");
+            for(var n = 0; n < 6; n++){
+                console.log("creating new square");
+                var $newSquare = $("<div>");
+                $newSquare.addClass("game-square");
+                $newColumn.append($newSquare);
+            };
+            $newColumn.addClass("game-column");
+            $("#game-board").append($newColumn);
+        };
+    };
+
+    createBoard();
 });
